efeeedd233152bbc6050e2995c9f934546ff20af
Vinylonprophet <915390118@qq.com>
2022-08-22 15:32:34 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -136,4 +136,9 @@ import { of, map, Observable, observable } from 'rxjs';
 //   },
 // });
 
+// 此部分总结
+// Observable 可以同時處理同步跟非同步行為
+// Observer 是一個物件，這個物件具有三個方法，分別是 next, error, complete
+// 訂閱一個 Observable 就像在執行一個 function
+
 // Open the console in the bottom right to see results.
