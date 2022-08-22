ac686490bfc1f21da800bb1dbc2051eb3efe2d7d
Vinylonprophet <915390118@qq.com>
2022-08-23 00:15:38 +0800

    1 file modified

--- finished/part9.ts
+++ finished/part9.ts
@@ -1,4 +1,14 @@
-import { concat, interval, last, of, skip, take, takeLast } from 'rxjs';
+import {
+  concat,
+  interval,
+  last,
+  merge,
+  of,
+  skip,
+  startWith,
+  take,
+  takeLast,
+} from 'rxjs';
 
 // Operators中的 skip, takeLast, last, concat, startWith, merge
 
@@ -58,3 +68,37 @@ import { concat, interval, last, of, skip, take, takeLast } from 'rxjs';
 // source3: (456)|
 //          concat()
 // example: ----0----1----2(3456)|
+
+// startWith
+// interval(1000)
+//   .pipe(startWith(0))
+//   .subscribe({
+//     next: function (value) {
+//       console.log(value);
+//     },
+//   });
+// 一开始塞了一个0,让example在一开始就送出0
+// source : ----0----1----2----3--...
+//   startWith(0)
+// example: (0)----0----1----2----3--...
+// startWith的值一开始就是同步发出,这个operator经常被用来保存起始状态
+
+// merge和concat一样都是用来合并observable的,但是和concat有很大不同
+// merge(interval(500).pipe(take(3)), interval(300).pipe(take(6))).subscribe({
+//   next: function (value) {
+//     console.log(value);
+//   },
+// });
+// merge是同时跑
+// source : ----0----1----2|
+// source2: --0--1--2--3--4--5|
+//             merge()
+// example: --0-01--21-3--(24)--5|
+
+// 今日总结:
+//   skip
+//   takeLast
+//   last
+//   concat
+//   startWith
+//   merge
