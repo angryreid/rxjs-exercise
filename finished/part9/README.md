be39d47e1cc80f642dc5a70a5e48a0988fede146
angryreid <onyourwill@gmail.com>
2022-08-22 22:42:19 +0800

    Feature: Added some advices

---
+++ finished/part9/README.md
@@ -0,0 +1,6 @@
+# TODO
+
+1. Each part should be a folder.
+2. Each folder should involve in multiple function feature.
+3. Every function feature should be independent and runnable.
+4. Remove all not using code.
--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import './style.css';
 import './finished/part5.ts';
-import './finished/part6.ts';
-import './finished/part7.ts';
-import './finished/part8.ts';
-
-import { of, map, Observable, observable } from 'rxjs';
-
-// Open the console in the bottom right to see results.
+// import './finished/part6.ts';
+// import './finished/part7.ts';
+// import './finished/part8.ts';
