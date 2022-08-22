983d00e1b6a461ad13b233f4c1dc9a24698b7425
Vinylonprophet <915390118@qq.com>
2022-08-22 15:18:21 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -36,4 +36,64 @@ import { of, map, Observable } from 'rxjs';
 // });
 // console.log('end');
 
+// 观察者
+// var observable = new Observable(function (observer) {
+//   observer.next('Jerry');
+//   observer.complete();
+//   observer.error();
+// 此处的next没有执行, 说明complete之后不会再执行next
+//   observer.next('看看是否执行');
+// });
+// var observer = {
+//   next: function (value) {
+//     console.log(value);
+//   },
+//   complete: function () {
+//     console.log('finish');
+//   },
+//   error: function (error) {
+//     console.log(error);
+//   },
+// };
+// observable.subscribe(observer);
+
+// 送出错误的案例
+// var observable = new Observable(function (observer) {
+//   try {
+//     observer.next('Jerry');
+//     observer.next('Tom');
+//     throw '错误';
+//     observer.next('看看是否执行');
+//   } catch (e) {
+//     console.log('报错信息:', Error);
+//   }
+// });
+// var observer = {
+//   next: function (value) {
+//     console.log(value);
+//   },
+//   error: function (error) {
+//     console.log('Error: ', error);
+//   },
+// };
+// observable.subscribe(observer);
+
+// 直接传入三个function
+// var observable = new Observable(function (observer) {
+//   observer.next('Jerry');
+//   observer.next('Tom');
+//   observer.complete();
+// });
+// observable.subscribe(
+//   (value) => {
+//     console.log(value);
+//   },
+//   (error) => {
+//     console.log('Error: ', error);
+//   },
+//   () => {
+//     console.log('complete');
+//   }
+// );
+
 // Open the console in the bottom right to see results.
