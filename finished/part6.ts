ae34b037cf1fbb1e55f9a311cccf685988da1eb8
Vinylonprophet <915390118@qq.com>
2022-08-22 16:32:38 +0800

    1 file modified

--- finished/part6.ts
+++ finished/part6.ts
@@ -10,6 +10,7 @@ import {
   throwError,
   interval,
   timer,
+  Observable,
 } from 'rxjs';
 
 // of是在我们想要同步传几个值的时候可以用它来简介表达
@@ -47,3 +48,69 @@ import {
 //     console.log(value);
 //   },
 // });
+// 看看promise呢
+// from(
+//   new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log('看看Promise');
+//     }, 3000);
+//   })
+// ).subscribe({
+//   next: function (value) {
+//     console.log(value);
+//   },
+// });
+
+// 我们也可以使用event建立observable
+// fromEvent(document.body, 'click').subscribe({
+//   next: function (value) {
+//     console.log('点击');
+//   },
+// });
+// fromEvent第一个参数是要传入DOM, 第一个是传入监听事件的名称,上方就是监听body是否呗点击,被点击就会答应出event
+
+// 补充一个fromEventPattern,这个是给类事件使用的,意思就是指行为和事件很像,同时有注册以及移除监听两种行为,就比如DOM有addEventListener和removeEventListener
+// 请出之前的代码
+// class Producer {
+//   listeners: any[];
+//   constructor() {
+//     this.listeners = [];
+//   }
+//   addListener(listener) {
+//     if (typeof listener === 'function') {
+//       this.listeners.push(listener);
+//       console.log('添加');
+//     } else {
+//       throw new Error('listener 必須是 function');
+//     }
+//   }
+//   removeListener(listener) {
+//     console.log('移除');
+//     this.listeners.splice(this.listeners.indexOf(listener), 1);
+//   }
+//   notify(message) {
+//     this.listeners.forEach((listener) => {
+//       listener(message);
+//     });
+//   }
+// }
+// var egghead = new Producer();
+// // 同时将注册监听和移除监听传入fromEventPattern建立Observable
+// // fromEventPattern(
+// //   (handler) => egghead.addListener(handler),
+// //   (handler) => egghead.removeListener(handler)
+// // ).subscribe({
+// //   next: function (value) {
+// //     console.log(value);
+// //   },
+// //   complete: function () {
+// //     console.log('finish');
+// //   },
+// // });
+// // egghead.notify('Hello! Can you hear me?');
+// fromEventPattern(
+//   // 注意不要直接将方法传入,避免this出错,也可以使用bind
+//   egghead.addListener.bind(egghead),
+//   egghead.removeListener.bind(egghead)
+// ).subscribe(console.log);
+
