e23da521acf089c3c2c61ca335fece3919bb0e71
Vinylonprophet <915390118@qq.com>
2022-08-23 10:57:56 +0800

    1 file modified, 1 added

---
+++ finished/part10.ts
@@ -0,0 +1,5 @@
+import { combineLatest, of } from 'rxjs';
+
+// Operators中的combineLatest, withLatestFrom, zip
+
+// combineLatest取到各个observable的最后的值再输出成一个值
--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import './finished/part6.ts';
 import './finished/part7.ts';
 import './finished/part8.ts';
 import './finished/part9.ts';
+import './finished/part10.ts';
 
 import { of, map, Observable, observable } from 'rxjs';
 
